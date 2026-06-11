20916dca1d841d0150af69f751d6e6aa261f8018
Sibaram S <sibaram917@gmail.com>
2026-06-11 21:58:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,48 @@
-resource "azurerm_resource_group" "RG1" {
-  name     = "lab-rg"
+resource "azurerm_resource_group" "rg" {
+  name     = "sr-lab-rg"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "VM-network"
-  location            = "West Europe"
-  resource_group_name = "lab-rg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
-  }
-
-  resource "azurerm_subnet" "subent" {
-  name                 = "example-subnet"
-  resource_group_name  = "lab-rg"
-  virtual_network_name = "VM-network"
-  address_prefixes     = ["10.0.1.0/24"]
-  }
-
-resource "azurerm_public_ip" "PIP" {
-  name                = "VMPIP"
-  resource_group_name = azurerm_resource_group.RG1.name
-  location            = azurerm_resource_group.RG1.location
-  allocation_method   = "Static"
 }
 
-  resource "azurerm_network_interface" "nic" {
+resource "azurerm_subnet" "subnet" {
+  name                 = "VM-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "VMPIP"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
   name                = "vm-nic"
-  location            = "West Europe"
-  resource_group_name = "lab-rg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subent.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.PIP.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
+
 resource "azurerm_network_security_group" "nsg" {
   name                = "vm-nsg"
-  location            = azurerm_resource_group.RG1.location
-  resource_group_name = azurerm_resource_group.RG1.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
     name                       = "Allow-SSH"
@@ -53,20 +55,20 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  }
+}
 
 resource "azurerm_subnet_network_security_group_association" "nsgassociation" {
-  subnet_id                 = azurerm_subnet.subent.id
+  subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 
-resource "azurerm_linux_virtual_machine" "VM" {
-  name                = "lab-machine"
-  resource_group_name = azurerm_resource_group.RG1.name
-  location            = azurerm_resource_group.RG1.location
-  size                = "Standard_B2ls_v2"
-  admin_username      = "adminuser"
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "sr-lab-machine"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B2ls_v2"
+  admin_username        = "adminuser"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   admin_password = "Password@123456"
